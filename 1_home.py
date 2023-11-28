7f9782ca359695b81d4964cd45749b08b6504bdc
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-11-28 21:37:15 +0100

    Update 1_home.py

--- 1_home.py
+++ 1_home.py
@@ -12,3 +12,37 @@ st.set_page_config(
 st.write("""
 # Anchetas 🛒 Navideñas 🎅🏻 Personalizadas!
 """)
+col1, col2 = st.columns(2)
+
+with col1:
+    suits = st.selectbox(
+        "Para cuántas personas será la Ancheta?",
+        ("1", "2", "3", "4", "5"),
+    )
+    rooms = st.radio(
+        "Producto Principal de la Ancheta?",
+        options=['Bebidas', 'Jamones','Quesos', 'Chocolates', 'Galletas'],
+    )
+    beds = st.selectbox(
+        "Producto Secundario de la Ancheta?",
+        ('Bebidas', 'Jamones','Quesos', 'Chocolates', 'Galletas'),
+    )
+
+with col2:
+    name = st.text_input('Escribe el nombre de quien regala la Ancheta', '''
+    ''')
+    email = st.text_input('Escribe el nombre de quien recibe la Ancheta', '''
+    ''')
+    bank_account =  st.text_input('Type your bank account:', '''
+    ''')
+    phone =  st.text_input('Type your phone number', '''
+    ''')
+    a = st.selectbox('Forma de Pago', ['PSE', 'Contra entrega', 'Crédito'])
+
+
+b = st.selectbox('Qué tan grande quieres la ancheta', ['Básica 100.000', 'Media 250.000', 'Grande 500.000€'])
+
+h = st.button('Crear Ancheta Personalizada 🛒 YA')
+
+if h:
+  st.write('Tu ancheta personalizada ha sido enviada a tu correo para pagos')
